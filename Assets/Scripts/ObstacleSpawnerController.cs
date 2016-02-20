1d70fd2fbb55b47ade41fcda5fd30f5cb260a79d
cor <cor@pruijs.nl>
2016-02-20 22:11:32 +0100

    Add project

---
+++ Assets/Scripts/ObstacleSpawnerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleSpawnerController : MonoBehaviour {
+
+    public GameObject obstacle;
+    public float spawnRange = 40f;
+    public float despawnDelay = 20f;
+    public int spawnsPerUpdate = 10;
+
+	// Use this for initialization
+	void Start () {
+     
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < spawnsPerUpdate; i++)
+        {
+            float randomX = Random.Range(-spawnRange, spawnRange);
+	        float randomZ = Random.Range(-spawnRange, spawnRange);
+       
+            Vector3 obstaclePosition = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+            GameObject spawnedObstacle = (GameObject)Instantiate(obstacle, obstaclePosition, transform.rotation);
+            Destroy(spawnedObstacle, despawnDelay);
+        }
+       
+       
+
+    
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody rb;
+    public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+        
+        rb = GetComponent<Rigidbody>();
+	   
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        rb.velocity = new Vector3(horizontalInput * speed, -30f, verticalInput * speed);
+	
+	}
+}
